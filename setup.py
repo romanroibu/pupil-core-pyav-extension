a6e28fd0fd089af08182be1ca151084bc63beb03
Pablo Prietz <pablo@prietz.org>
2020-10-30 16:27:53 +0100

    cimport av test

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 import setuptools
+from Cython.Build import cythonize
 
-setuptools.setup()
+test = setuptools.Extension(
+    name="pupil_av.test",
+    sources=["src/pupil_av/test.pyx"],
+    include_dirs=[],
+)
+
+setuptools.setup(ext_modules=cythonize([test]))
---
+++ src/pupil_av/test.pyx
@@ -0,0 +1,4 @@
+from av.buffer cimport Buffer
+
+cdef Buffer test_buffer = Buffer()
+print(test_buffer)
